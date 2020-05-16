829d1f327b2c5bcb39f2283595e071dabd2e7a23
Rob Dey <iarunpaul@yahoo.com>
2020-05-16 18:38:58 +0000

    Updated flash message styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   belongs_to :plan   
   
   attr_accessor :stripe_card_token
+  
   # If Pro user passes the validation for email, password etc. then call Stripe
   # and tell Stripe to add a subscription by charging customer's card
   # Stripe then returns a customer token in response 
