6acace76b66790d09d157401c7c6289381441325
Rob Dey <iarunpaul@yahoo.com>
2020-05-16 08:47:51 +0000

    Finally added the plan through hidden form fields for user sign up and updated application controller, registration controller, user model and routes.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,20 +1,14 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  def new
-    
-  end
-  
   def create
     super do |resource|
-      # if params[:plan]
-        # resource.plan_id = params[:plan]
+      if params[:plan]
+        resource.plan_id = params[:plan]
         if resource.plan_id == 2
-          resource.save
+          resource.save_with_subscription
         else
-          
           resource.save
-          
         end
-      # end
+      end
     end
   end
-end  
+end
