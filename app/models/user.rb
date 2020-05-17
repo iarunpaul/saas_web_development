e98e8afface19dc4fae8d39b78c37a861b33b77d
Rob Dey <iarunpaul@yahoo.com>
2020-05-17 06:24:16 +0000

    Added profile and user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  belongs_to :plan   
+  belongs_to :plan
+  has_one :profile
   
   attr_accessor :stripe_card_token
   
