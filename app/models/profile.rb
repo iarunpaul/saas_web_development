1ed8db2c890ef1c10acbae216fd90405bd7a1d16
Rob Dey <iarunpaul@yahoo.com>
2020-05-19 15:45:34 +0000

    Added for field for image upload and updated show view to display the pro pic; Done corrects for typos in the Profile model and migration

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,6 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   has_attached_file     :avatar,
                         :styles => { :medium => "300x300>", :thump => "100x100>" },
-                        :defaul_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/*\Z/
+                        :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
