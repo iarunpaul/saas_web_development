9b93659ab38f1a1b1715f4b39bb10a7f248f4f41
Rob Dey <iarunpaul@yahoo.com>
2020-05-20 03:10:26 +0000

    Added profile update method to profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,7 +14,7 @@ class ProfilesController < ApplicationController
         @profile = @user.build_profile( profile_params )
         if @profile.save
             flash[:success] = "Profile has been created"
-            redirect_to user_path(params[:user_id])
+            redirect_to user_path(id: params[:user_id])
         else
             render action: :new
         end    
@@ -22,9 +22,26 @@ class ProfilesController < ApplicationController
     
     # GET request view for /users/:user_id/profile/edit
     def edit
-        
+        # Retrieving the user and assigning the user profile to @profile
         @profile = User.find(params[:user_id]).profile
     end
+    
+    # PATCH request for user profile /users/:user_id/profile
+    def update
+        # Retrieving the user with the current user id
+        @user = User.find(params[:user_id])
+        # Retrieving that user's profile and assigning to @profile
+        @profile = @user.profile
+        # Whitelisting and saving the attributes(update only)
+        if @profile.update_attributes(profile_params)
+           flash[:success] = "Profile Updated!"
+        # Sending back the user to user show(user profile view)
+           redirect_to user_path(id: params[:user_id])
+        else
+        # If update fails rendering the edit page  
+            render action: :edit
+        end   
+    end    
     private
         def profile_params
             params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :email, :description)
