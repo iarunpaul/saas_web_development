6c5435358e49e4132a9c30627f76b4298381bb5b
Rob Dey <iarunpaul@yahoo.com>
2020-05-20 03:57:29 +0000

    Added index page of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
     
+    def index
+        
+    end
+    
     def show
         # GET to /users/:id
         @user = User.find(params[:id])
