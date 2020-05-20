ca746e7b84da713f6013bbd4fbc8f479b2dc68af
Rob Dey <iarunpaul@yahoo.com>
2020-05-20 13:17:23 +0000

    Added loop for user display on index page and styled page and optimized Active record query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
     before_action :authenticate_user!
     
     def index
-        
+        @users = User.includes(:profile)
+      
+       
     end
     
     def show
